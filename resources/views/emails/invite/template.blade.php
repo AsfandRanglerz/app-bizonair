0b1669b8b2144e8c9397613c7f87d8468e8e6e9b
AsfandRanglerz <asfand.ranglerz@gmail.com>
2022-01-27 18:43:17 +0500

    change

--- resources/views/emails/invite/template.blade.php
+++ resources/views/emails/invite/template.blade.php
@@ -9,7 +9,7 @@
     <h5 style="margin: 0">For Mobile App Users:</h5>
     <p style="margin-bottom: 16px">Please use this verification code {{$invite->verification_code}} while doing registration process or if you're are registered member then just swipe your sidebar or drawer and click the link as shown in attached Image below, on clicking that link OTP Screen will appear where you can add the OTP.</p>
     <img src="{{$ASSET}}/front_site/images/otp-screen.png" /><br>
-    <a href="" target="_blank" style="font-size: 16px">Video Tutorial</a>
+    <a href="https://drive.google.com/file/d/1bZZywWMhMVqwuWcDYsMxeheYPAgNLnAB/view?usp=sharing" target="_blank" style="font-size: 16px">Video Tutorial</a>
 </div>
 <p style="font-size: 16px">Thanks,<br>{{ config('app.name') }}</p>
 @endcomponent
