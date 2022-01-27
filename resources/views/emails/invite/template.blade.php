04b97a5f857db8abd72ea33f0c8e50e5aebf3711
AsfandRanglerz <asfand.ranglerz@gmail.com>
2022-01-27 16:56:04 +0500

    change

--- resources/views/emails/invite/template.blade.php
+++ resources/views/emails/invite/template.blade.php
@@ -1,9 +1,15 @@
 @component('mail::message')
-# Hi,
 <?php  $company =\App\UserCompany::where('company_id',$invite->company_id)->first(); ?>
-{{ucfirst($user->name)}} has invited to Join Biz Office "{{ $company->company_name }}". For Website user please <a target="_blank" href="{{'https://www.bizonair.com/accept-token/'.$invite->token.'/'.$user->email}}">Click here</a> Or for mobile app user then use this verification code   {{$invite->verification_code}} while registering user  to activate!
-
-
-Thanks,<br>
-{{ config('app.name') }}
+<!-- {{ucfirst($user->name)}} has invited to Join Biz Office "{{ $company->company_name }}". For Website user please <a target="_blank" href="{{'https://www.bizonair.com/accept-token/'.$invite->token.'/'.$user->email}}">Click here</a> Or for mobile app user then use this verification code   {{$invite->verification_code}} while registering user  to activate! -->
+<div>
+    <h5 style="margin: 0">Intro:</h5>
+    <p style="margin-bottom: 16px">{{ucfirst($user->name)}} has invited to Join Biz Office "{{ $company->company_name }}".</p>
+    <h5 style="margin: 0">For Web Users:</h5>
+    <p style="margin-bottom: 16px">Please visit this link <a target="_blank" href="{{'https://www.bizonair.com/accept-token/'.$invite->token.'/'.$user->email}}">Click here</a> for further processing.</p>
+    <h5 style="margin: 0">For Mobile App Users:</h5>
+    <p style="margin-bottom: 16px">Please use this verification code {{$invite->verification_code}} while doing registration process or if you're are registered member then just swipe your sidebar or drawer and click the link as shown in attached Image below, on clicking that link OTP Screen will appear where you can add the OTP.</p>
+    <img src="{{$ASSET}}/front_site/images/otp-screen.png" /><br>
+    <a href="" target="_blank" style="font-size: 16px">Video Tutorial</a>
+</div>
+<p style="font-size: 16px">Thanks,<br>{{ config('app.name') }}</p>
 @endcomponent
