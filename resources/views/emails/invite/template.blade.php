d5e7dec65ad9cd385aa4afbf531c835d683888c2
AsfandRanglerz <asfand.ranglerz@gmail.com>
2022-01-28 10:59:48 +0500

    change

--- resources/views/emails/invite/template.blade.php
+++ resources/views/emails/invite/template.blade.php
@@ -7,7 +7,7 @@
     <h5 style="margin: 0">For Web Users:</h5>
     <p style="margin-bottom: 16px">Please visit this link <a target="_blank" href="{{'https://www.bizonair.com/accept-token/'.$invite->token.'/'.$user->email}}">Click here</a> for further processing.</p>
     <h5 style="margin: 0">For Mobile App Users:</h5>
-    <p style="margin-bottom: 16px">Please use this verification code {{$invite->verification_code}} while doing registration process or if you're are registered member then just swipe your sidebar or drawer and click the link as shown in attached Image below, on clicking that link OTP Screen will appear where you can add the OTP.</p>
+    <p style="margin-bottom: 16px">Please use this verification code {{$invite->verification_code}} while doing registration process or if you're a registered member then just swipe your sidebar or drawer and click the link as shown in attached Image below, on clicking that link OTP Screen will appear where you can add the OTP.</p>
     <img src="{{$ASSET}}/front_site/images/otp-screen.png" /><br>
     <a href="https://drive.google.com/file/d/1bZZywWMhMVqwuWcDYsMxeheYPAgNLnAB/view?usp=sharing" target="_blank" style="font-size: 16px">Video Tutorial</a>
 </div>
